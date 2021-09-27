3ceb4cb3329f3415ff10859d136a6853908e74ba
OskarAa <90186369+OskarAa@users.noreply.github.com>
2021-09-27 21:10:02 +0300

    Update rekins.py

--- rekins.py
+++ rekins.py
@@ -1,9 +1,9 @@
 class Rekins:
 
-    def __init__(self, vards, teksts, garums, platums, augstums, materials):
-        self.vards = vards
-        self.teksts = teksts
-        self.garums =garums
-        self.platums = platums
-        self.augstum = augstums
-        self.materials = materials
+    def __init__(self,name,text,height,width,material,length):
+        self.name = name
+        self.text = text
+        self.height = height
+        self.width = width
+        self.material = material
+        self.length = length
