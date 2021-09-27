34dfb5654f57e8d16e625c301d93bd17af363489
OskarAa <90186369+OskarAa@users.noreply.github.com>
2021-09-27 21:09:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,61 @@
 import rekins
 
 def print_info():
-    print('programma rekina sagatavosana')
+
+    txt = ('\033[94m{}\033[0m'.format("[Rēķinu izrakstīšanas programma]"))
+    INF = txt.center(65)
+    print(INF)
+
+def user_input():
+    print("\033[94m{}\033[0m".format("[Programma, lai uzdotu pāris jautājumus un iestatītu galīgo rēķinu]"))
+
+    name = input("Jusu vārds: ")
+    text = input("Jūsu teksts: ")
+    height = float(input("Cik augstu kaste vēlas? "))
+    width = float(input("Cik platu kastīti vēlaties? (tikai veseli skaitļi): "))
+    material = float(input("materiāla cena: "))
+    length = float(input("Cik garumu kaste vēlas? "))
+
+    darba_samaksa = 15
+    PVN = 21
+
+    produkta_cena = width/100.0 * length/100*height/100/3*material
+    PVN_summa = produkta_cena + darba_samaksa * PVN / 100
+    rekina_summa = produkta_cena + darba_samaksa + PVN_summa
 
 
-def lietotaja_ievads():
-    vards = input('Ieraksti savu vardu:')
-    teksts = input('Ieraksti savu tekstu:')
-    kokmateriala_cena = input('Kokmateriala cena')
-    platums = input('Cik platu kastisti velies?: (Tikai veselie skaitli)')
-    garums = input('Cik izmers? Garums: (mm)')
-    augstums = input('Cik izmers? Augstums: (mm)')
+    f = open("rekins.txt", "w")
+    f.write("Datums: " + format(today))
+    f.write("\n")
+    f.write("Jusu vards: " + format(name))
+    f.write("\n")
+    f.write("Jusu teksts: " + format(text))
+    f.write("\n")
+    f.write("Produkta cena: " + format(produkta_cena, ",.2f"))
+    f.write("\n")
+    f.write("PVN summa: " + format(PVN_summa, ",.2f"))
+    f.write("\n")
+    f.write("Rekina summa: " + format(rekina_summa, ",.2f"))
+    f.close()
 
-    return vards, teksts, platums, garums, augstums, kokmateriala_cena
+    print("\033[94m{}\033[0m".format("--------------------------------"))
+    print("Produkta cena: €" + format(produkta_cena, ",.2f"))
+    print("PVN summa: €" + format(PVN_summa, ",.2f"))
+    print("Rekina summa: €" + format(rekina_summa, ",.2f"))
+    print("\033[94m{}\033[0m".format("--------------------------------"))
 
+
+from datetime import date
+
+today = date.today()
+print("Šodienas datums:", "[", today , "]")
 
 if __name__ == '__main__':
     print_info()
-    while 1>0:
-        lietotaja_ievads()
-        rekins = rekins.Rekins()
-        run_again = input("Vai velies sagatavot reikinu velreiz?", ja = 1, ne = 0)
+    while True:
+       user_input()
+       restart = input('Vai vēlaties izveidot jaunu? (Y/N) ').lower()
+       if restart == "y":
+           print_info()
+       else:
+           break
